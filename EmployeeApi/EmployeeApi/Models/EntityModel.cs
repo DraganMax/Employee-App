cb14741687f7db7edb2ae7303903df8c0556f700
DraganMax <maksims.dragans@gmail.com>
2021-06-27 12:06:09 +0300

    Added EntityModel

---
+++ EmployeeApi/EmployeeApi/Models/EntityModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeApi.Models
+{
+    public class EntityModel
+    {
+        public int Id { get; set; }
+    }
+}
